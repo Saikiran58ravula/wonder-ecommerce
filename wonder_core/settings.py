b50ecf845d8fe2f01c82d92938cdfade1d134101
Saikiran Ravula <saikiransunny58@gmail.com>
2026-09-15 18:30:46 +0530

    Add Category and Product models

--- wonder_core/settings.py
+++ wonder_core/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Wonder apps
+    'accounts',
+    'products',
+    'cart',
+    'orders',
 ]
 
 MIDDLEWARE = [
