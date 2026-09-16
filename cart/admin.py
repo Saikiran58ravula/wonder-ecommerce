e4df6bddbb1d2cb642d32dcedd9e0f2889683ebf
Saikiran Ravula <saikiransunny58@gmail.com>
2026-09-16 11:50:12 +0530

    Add persistent shopping cart with add/update/remove functionality

--- cart/admin.py
+++ cart/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Cart, CartItem
 
-# Register your models here.
+
+class CartItemInline(admin.TabularInline):
+    model = CartItem
+    extra = 0
+
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'session_key', 'created_at')
+    inlines = [CartItemInline]
--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,58 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
+from products.models import Product
+from .models import Cart, CartItem
 
-# Create your views here.
+
+def get_or_create_cart(request):
+    if request.user.is_authenticated:
+        cart, created = Cart.objects.get_or_create(user=request.user)
+    else:
+        if not request.session.session_key:
+            request.session.create()
+        session_key = request.session.session_key
+        cart, created = Cart.objects.get_or_create(session_key=session_key, user=None)
+    return cart
+
+
+def add_to_cart(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    cart = get_or_create_cart(request)
+
+    cart_item, created = CartItem.objects.get_or_create(cart=cart, product=product)
+    if not created:
+        cart_item.quantity += 1
+        cart_item.save()
+
+    messages.success(request, f"{product.name} added to cart.")
+    return redirect('products:product_detail', slug=product.slug)
+
+
+def view_cart(request):
+    cart = get_or_create_cart(request)
+    context = {'cart': cart}
+    return render(request, 'cart/cart.html', context)
+
+
+def update_cart_item(request, item_id):
+    cart_item = get_object_or_404(CartItem, id=item_id)
+    action = request.POST.get('action')
+
+    if action == 'increase':
+        cart_item.quantity += 1
+        cart_item.save()
+    elif action == 'decrease':
+        cart_item.quantity -= 1
+        if cart_item.quantity <= 0:
+            cart_item.delete()
+        else:
+            cart_item.save()
+
+    return redirect('cart:view_cart')
+
+
+def remove_from_cart(request, item_id):
+    cart_item = get_object_or_404(CartItem, id=item_id)
+    cart_item.delete()
+    messages.success(request, "Item removed from cart.")
+    return redirect('cart:view_cart')
