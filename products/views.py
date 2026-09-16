0442fce2c6ba4d052557da5af0b2fad330e5f7ab
Saikiran Ravula <saikiransunny58@gmail.com>
2026-09-16 09:52:15 +0530

    Add public storefront: home, product list, and product detail pages

--- products/views.py
+++ products/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Category, Product
 
-# Create your views here.
+
+def home(request):
+    categories = Category.objects.all()
+    featured_products = Product.objects.filter(is_active=True)[:8]
+    context = {
+        'categories': categories,
+        'featured_products': featured_products,
+    }
+    return render(request, 'products/home.html', context)
+
+
+def product_list(request):
+    products = Product.objects.filter(is_active=True)
+    categories = Category.objects.all()
+
+    category_slug = request.GET.get('category')
+    if category_slug:
+        products = products.filter(category__slug=category_slug)
+
+    query = request.GET.get('q')
+    if query:
+        products = products.filter(name__icontains=query)
+
+    context = {
+        'products': products,
+        'categories': categories,
+        'selected_category': category_slug,
+        'query': query or '',
+    }
+    return render(request, 'products/product_list.html', context)
+
+
+def product_detail(request, slug):
+    product = get_object_or_404(Product, slug=slug, is_active=True)
+    related_products = Product.objects.filter(
+        category=product.category, is_active=True
+    ).exclude(id=product.id)[:4]
+    context = {
+        'product': product,
+        'related_products': related_products,
+    }
+    return render(request, 'products/product_detail.html', context)
