0d0f684921db565489595a88b12cfb0905966c90
Saikiran Ravula <saikiransunny58@gmail.com>
2026-09-20 17:34:55 +0530

    Switch to non-strict WhiteNoise storage to fix collectstatic build failure

--- wonder_core/settings.py
+++ wonder_core/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # Media files (product images) - stored on Cloudinary, not local disk
 MEDIA_URL = '/media/'
