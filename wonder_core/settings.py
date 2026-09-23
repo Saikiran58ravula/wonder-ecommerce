892ee07f58504d230edd1f4bf5cc5667d23df9cc
Saikiran Ravula <saikiransunny58@gmail.com>
2026-09-23 09:47:16 +0530

    Fix INSTALLED_APPS order: staticfiles before cloudinary_storage per official docs

--- wonder_core/settings.py
+++ wonder_core/settings.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cloudinary_storage',
     'cloudinary',
 
     # Wonder apps
